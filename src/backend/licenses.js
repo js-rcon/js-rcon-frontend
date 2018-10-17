0f6a7519f4cbeee928ee763e9d6eb8625c6ddc89
Linus Willner <lwtechgaming@gmail.com>
2018-10-17 20:58:48 +0300

    Add missing Material icons license

--- src/backend/licenses.js
+++ src/backend/licenses.js
@@ -74,6 +74,26 @@ export const licenses = {
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.`
   },
+  materialIcons: {
+    title: 'Material icons',
+    content: `Apache License
+    Version 2.0, January 2004
+    http://www.apache.org/licenses/
+
+    Copyright 2014-present Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+ 
+        http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`
+  },
   react: {
     title: 'React',
     content: `MIT License
